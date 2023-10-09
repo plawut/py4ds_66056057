d481a576146f47eed121656d9ee5a87f0d8f9b59
Polawut K <66056057@kmitl.ac.th>
2023-10-09 18:41:11 +0700

    Fix ex13

--- src/ex13.py
+++ src/ex13.py
@@ -3,7 +3,7 @@ Exercise 13
 """
 
 
-def calc_sum(params):
+def calc_sum(num_list):
     """
     Calculate the sum of a list of numbers.
 
@@ -13,10 +13,15 @@ def calc_sum(params):
     Returns:
     - int: The sum of all the numbers in the list.
     """
-    pass
-
-
-def calc_prod(params):
+    #FIX
+    result = 0
+    if num_list == []:
+        return 0
+    else:
+        for i in num_list:
+            result = i + result
+        return result
+def calc_prod(num_list):
     """
     Calculates the product of all the numbers in the given list.
 
@@ -26,4 +31,11 @@ def calc_prod(params):
     Returns:
         int: The product of all the numbers in the list.
     """
-    pass
+    #FIX
+    result = 1
+    if num_list == []:
+        return 1
+    else:
+        for n in num_list:
+            result = result*n
+        return result
