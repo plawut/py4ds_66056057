d57ef620381770799907942b6120f1fc348e8c95
Polawut K <66056057@kmitl.ac.th>
2023-10-09 18:46:27 +0700

    Fix ex14

--- src/ex14.py
+++ src/ex14.py
@@ -14,4 +14,11 @@ def average(num_list: list) -> float:
     - float: The average of the numbers in the list.
       If the list is empty, returns 0.
     """
-    pass
+    #FIX
+    sum_result = 0
+    for i in num_list:
+        sum_result += i
+    if sum_result > 0:
+        return sum_result/len(num_list)
+    else:
+        return 0
