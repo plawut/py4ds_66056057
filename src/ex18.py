1687054ada5ec915c50f545d0c5be222ad93e4e6
Polawut K <66056057@kmitl.ac.th>
2023-10-09 20:08:00 +0700

    Fix ex18

--- src/ex18.py
+++ src/ex18.py
@@ -3,5 +3,20 @@ Exercise 18 : Buy 8 get 1 free
 """
 
 
-def get_cost_of_coffee(param, param1):
-    pass
+def get_cost_of_coffee(num, price):
+    #FIX
+    if num > 8:
+        discount = num//8
+        total = price*num - discount*price
+        return total
+    else:
+        return num*price
+
+def get_cost_of_coffee_2(num, price):
+    #FIX
+    if num > 8:
+        discount = num//8
+        total = price*num - discount*price
+        return total
+    else:
+        return num*price
