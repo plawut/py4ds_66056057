c3ab5459b2544014b981e58abf053a31b351df4d
Polawut K <66056057@kmitl.ac.th>
2023-10-09 18:25:40 +0700

    Fix ex11

--- src/ex11.py
+++ src/ex11.py
@@ -3,7 +3,7 @@ Exercise 11
 """
 
 
-def get_hr_min_sec(param):
+def get_hr_min_sec(tsec):
     """
     Generates a string representation of the given number
     of seconds in the format "9h 9m 9s".
@@ -22,4 +22,25 @@ def get_hr_min_sec(param):
         >>> get_hr_min_sec(0)
         '0s'
     """
-    pass
+    #FIX
+    if tsec < 60:
+        return str(tsec)+'s'
+    elif (tsec >= 60) and (tsec < 3600):
+        x = tsec//60
+        y = tsec%60
+        if y != 0:
+            return str(x)+'m'+' '+str(y)+'s'
+        else:
+            return str(x)+'m'
+    else:
+        a = tsec//3600
+        b = tsec%3600
+        if b > 60:
+            c = b//60
+            d = b%60
+            return str(a)+'h' + ' ' + str(c)+'m' + ' ' + str(d) + 's'
+        elif (b>0) and (b<=60):
+            return str(a)+'h' + ' ' + str(b) + 's'
+        else:
+            return str(a)+'h'
+
